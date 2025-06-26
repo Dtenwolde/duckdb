80bab6bc1c578865d493100cbbdc94b69121fe6f
dtenwolde <dljtw98@gmail.com>
2025-06-26 10:34:39 +0200

    Trying to get transformer factory to work

--- extension/autocomplete/include/transformer/peg_transformer.hpp
+++ extension/autocomplete/include/transformer/peg_transformer.hpp
@@ -1,48 +1,84 @@
 #pragma once
 
 #include "parse_result.hpp"
-#include "duckdb/common/case_insensitive_map.hpp"
+#include "matcher.hpp"
+#include "parser/peg_parser.hpp"
+#include "duckdb/parser/statement/set_statement.hpp"
+
 
 #include <functional>
-#include <unordered_map>
 
 namespace duckdb {
 
+struct PEGTransformerState {
+	explicit PEGTransformerState(vector<MatcherToken> &tokens)
+		: tokens(tokens), token_index(0) {
+	}
+	explicit PEGTransformerState(MatchState &state)
+		: tokens(state.tokens), token_index(state.token_index) {
+	}
+
+	vector<MatcherToken> &tokens;
+	idx_t token_index;
+};
+
 class PEGTransformer {
 public:
-	using TransformFunction = std::function<unique_ptr<ParseResult>(PEGTransformer &, ParseResult &)>;
+	using TransformFunction = std::function<reference<SQLStatement>(PEGTransformer &)>;
+
+	PEGTransformer(Allocator &allocator, PEGTransformerState &state,
+	               string_map_t<TransformFunction> &rules)
+	    : allocator(allocator), state(state), rules(rules) {
+	}
 
 public:
-	// Register a rule
-	void AddRule(const string &rule_name, TransformFunction function);
-
-	static unique_ptr<PEGTransformer> RootTransformer();
-
-	// Apply a rule
-	unique_ptr<ParseResult> Transform(const string &rule_name, ParseResult &input);
-
-	static unique_ptr<ParseResult> TransformStatement(ParseResult &input);
-	static unique_ptr<ParseResult> TransformUseStatement(ParseResult &input);
-
-
-	// Create a new result
-	template <class T, class... Args>
-	T &Make(Args &&...args) {
-		auto result = make_uniq<T>(std::forward<Args>(args)...);
-		auto &ref = *result;
-		allocated.emplace_back(std::move(result));
+	template <class T, class... ARGS>
+	reference<SQLStatement> Make(ARGS &&... args) {
+		static_assert(std::is_base_of<SQLStatement, T>::value, "T must be a SQLStatement");
+		auto ptr = make_uniq<T>(*this, std::forward<ARGS>(args)...);
+		reference<T> ref = ptr.get();
+		owned_results.push_back(std::move(ptr));
 		return ref;
 	}
 
+	template <class T>
+	T &Children() {
+		if (!current_parse_result || current_parse_result->Type() == ParseResultType::INVALID) {
+			throw InternalException("PEGTransformer::Children() called on an unexpected state");
+		}
+		return current_parse_result->Cast<T>();
+	}
+
+	static reference<ParseResult> RootTransformer(PEGTransformerState &state, Allocator &allocator);
+
+	// Apply a rule
+	reference<ParseResult> Transform(const string_t &rule_name);
+
+	static unique_ptr<SQLStatement> TransformStatement(PEGTransformer &transformer);
+	static unique_ptr<SetStatement> TransformUseStatement(PEGTransformer &transformer);
+
+
+public:
+	unique_ptr<ParseResult> current_parse_result;
+	Allocator &allocator;
+	PEGTransformerState &state;
+	vector<unique_ptr<ParseResult>> owned_results;
 
 private:
-	case_insensitive_map_t<TransformFunction> rules;
-	vector<reference<ParseResult>> allocated;
+	string_map_t<TransformFunction> &rules;
 };
 
 class PEGTransformerFactory {
 public:
-	static unique_ptr<PEGTransformer> Create(const char *grammar, const char *root_rule);
+	static reference<ParseResult> CreateTransformer(const char *grammar, const char *root_rule, PEGTransformerState &state, Allocator &allocator);
+
+	string_map_t<PEGTransformer::TransformFunction> transform_functions = {
+		{"UseStatement", PEGTransformer::TransformUseStatement}
+	};
+
+private:
+	reference<ParseResult> CreateTransformer(PEGParser &parser, const string_t &rule_name, PEGTransformerState &state, Allocator &allocator);
+
 };
 
 } // namespace duckdb
