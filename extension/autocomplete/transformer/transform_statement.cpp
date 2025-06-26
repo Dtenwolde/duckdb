0d638c9eb634dd236043320b3cfe1fd2efc9eb00
dtenwolde <dljtw98@gmail.com>
2025-06-26 10:34:52 +0200

    First implementation of statement and usestatement transformers

--- extension/autocomplete/transformer/transform_statement.cpp
+++ extension/autocomplete/transformer/transform_statement.cpp
@@ -1,10 +1,17 @@
 #include "duckdb/common/printer.hpp"
+#include "duckdb/parser/sql_statement.hpp"
 #include "transformer/peg_transformer.hpp"
 
 namespace duckdb {
 
-unique_ptr<ParseResult> PEGTransformer::TransformStatement(ParseResult &input) {
-	Printer::Print("Got here");
-	return {};
+reference<SQLStatement> PEGTransformer::TransformStatement(PEGTransformer &self) {
+	throw NotImplementedException("TransformStatement");
+	// auto &choice = self.parse_results<ChoiceParseResult>();
+	// switch (choice.selected_idx) {
+	// case 0:
+	// 	return self.TransformUseStatement(self);
+	// default:
+	// 	throw NotImplementedException("TransformStatement has not been implemented yet.");
+	// }
 }
 } // namespace duckdb
--- extension/autocomplete/transformer/transform_use.cpp
+++ extension/autocomplete/transformer/transform_use.cpp
@@ -1,32 +1,19 @@
-// transform_use_statement.cpp
 #include "transformer/parse_result.hpp"
 #include "duckdb/common/helper.hpp"
+#include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb/parser/statement/set_statement.hpp"
 
 namespace duckdb {
 
-unique_ptr<ParseResult> PEGTransformer::TransformUseStatement(ParseResult &input) {
-	// UseStatement <- 'USE'i DottedIdentifier
-	auto &list = input.Cast<ListParseResult>();
-	if (list.parse_result.size() != 2) {
-		throw InternalException("Invalid number of children for UseStatement");
-	}
-	auto &first_child = list.GetChild<KeywordParseResult>(0);
-	auto &second_child = list.GetChild<ListParseResult>(1);
-
-	return {};
-	// Skip the 'USE' keyword
-	// auto &keyword = list.parse_result[0].get();
-	// if (keyword.type != ParseResultType::KEYWORD) {
-		// throw InternalException("Expected KEYWORD as first child in UseStatement");
-	// }
-
-	// Second child should be a string literal (DottedIdentifier -> CatalogName or SchemaName -> StringLiteral)
-	// auto &identifier = GetChild<IdentifierParseResult>(list.parse_result[1]);
-
-	// auto stmt = make_uniq<UseStatement>();
-	// stmt->schema = identifier.identifier;
-
-	// return Make<StatementParseResult>(std::move(stmt));
+// src/parser/transform/statement/transform_use.cpp
+reference<SetStatement> PEGTransformer::TransformUseStatement(PEGTransformer &self) {
+	// UseStatement <- 'USE'i UseTarget
+	auto &children = self.Children<ListParseResult>();
+	auto &use_target = children.Child<ListParseResult>(1);
+	// TODO Should be changed below this
+	auto name_expr = make_uniq<ConstantExpression>(Value("foo"));
+	return self.Make<SetStatement>("schema", std::move(name_expr), SetScope::AUTOMATIC);
 }
+
 
 } // namespace duckdb
