47fa63cc548d7cea0d8f6fac42ae86f77989194e
dtenwolde <dljtw98@gmail.com>
2025-07-21 15:20:55 +0200

    Remove simplified code in comment

--- extension/autocomplete/parser/peg_parser_override.cpp
+++ extension/autocomplete/parser/peg_parser_override.cpp
@@ -7,47 +7,6 @@ namespace duckdb {
 struct MatcherToken;
 
 PEGParserOverride::PEGParserOverride() {
-	/*
-	    const char grammar[] = {
-	        "List(D) <- D (',' D)* ','?\n"
-	        "Statement <- UseStatement / DeleteStatement / SetStatement / ResetStatement\n"
-	        "UseStatement <- 'USE'i DottedIdentifier\n"
-	        "DeleteStatement <- 'DELETE'i Identifier\n"
-	        "UseTarget <- DottedIdentifier\n"
-	        "SetStatement <- 'SET'i (StandardAssignment / SetTimeZone)\n"
-	        "StandardAssignment <- (SetVariable / SetSetting) SetAssignment\n"
-	        "SetTimeZone <- 'TIME'i 'ZONE'i Expression\n"
-	        "SetSetting <- SettingScope? SettingName\n"
-	        "SetVariable <- VariableScope SettingName\n"
-	        "SettingName <- Identifier\n"
-	        "SettingScope <- LocalScope / SessionScope / GlobalScope\n"
-	        "LocalScope <- 'LOCAL'i\n"
-	        "SessionScope <- 'SESSION'i\n"
-	        "GlobalScope <- 'GLOBAL'i\n"
-	        "VariableScope <- 'VARIABLE'i\n"
-	        "SetAssignment <- VariableAssign VariableList\n"
-	        "VariableAssign <- '=' / 'TO'\n"
-	        "VariableList <- List(Expression)\n"
-	        "Expression <- Identifier\n"
-	        "ResetStatement <- 'RESET'i (SetVariable / SetSetting)\n"
-	        "DottedIdentifier <- Identifier ('.' Identifier)*\n"
-	        "Identifier <- [a-z_A-Z]\n"
-	    };
-
-	    */
-	/*const char grammar[] = {
-	    "List(D) <- D (',' D)* ','?\n"
-	    "Parens(D) <- '(' D ')'\n"
-	    "Identifier <- [a-z_A-Z]\n"
-	    "IdentParen <- Parens(Identifier? Identifier)\n"
-	    "FilterClause <- Parens('WHERE'i? Identifier)\n"
-	    "IdentList <- List(Identifier)\n"
-	    "ExcludeList <- 'EXCLUDE'i (Parens(List(ExcludeName)) / ExcludeName)\n"
-	    "IdentListParen <- Parens(List(Identifier))\n"
-	    "DottedIdentifier <- Identifier ('.' Identifier)*\n"
-	};
-	*/
-
 	auto grammar = const_char_ptr_cast(INLINED_PEG_GRAMMAR);
 	factory = make_uniq<PEGTransformerFactory>(grammar);
 }
