89664bcb92331b49dfb50638f4cc5aea6aa07de5
dtenwolde <dljtw98@gmail.com>
2025-07-30 11:24:25 +0200

    Update pragma statement

--- extension/autocomplete/transformer/transform_pragma.cpp
+++ extension/autocomplete/transformer/transform_pragma.cpp
@@ -5,7 +5,7 @@ unique_ptr<SQLStatement> PEGTransformerFactory::TransformPragmaStatement(PEGTran
                                                                          optional_ptr<ParseResult> parse_result) {
 	// Rule: PragmaStatement <- 'PRAGMA'i (PragmaAssign / PragmaFunction)
 	auto &list_pr = parse_result->Cast<ListParseResult>();
-	auto &matched_child = list_pr.Child<ChoiceParseResult>(1);
+	auto &matched_child = list_pr.Child<ListParseResult>(1).Child<ChoiceParseResult>(0);
 	return transformer.Transform<unique_ptr<SQLStatement>>(matched_child.result);
 }
 
@@ -13,10 +13,15 @@ unique_ptr<SQLStatement> PEGTransformerFactory::TransformPragmaAssign(PEGTransfo
 																		optional_ptr<ParseResult> parse_result) {
 	// Rule: PragmaAssign <- SettingName '=' Expression
 	auto &list_pr = parse_result->Cast<ListParseResult>();
-	auto name = transformer.Transform<string>(list_pr.children[0]);
-	auto value = transformer.Transform<unique_ptr<ParsedExpression>>(list_pr.children[2]);
-	auto result = make_uniq<SetVariableStatement>(std::move(name), std::move(value), SetScope::AUTOMATIC);
-	return result;
+	auto result = make_uniq<PragmaStatement>();
+	auto &info = *result->info;
+	info.name = list_pr.Child<IdentifierParseResult>(0).identifier;
+	auto value = transformer.Transform<unique_ptr<ParsedExpression>>(list_pr.Child<ListParseResult>(2));
+	info.parameters.push_back(std::move(value));
+
+	auto set_statement =
+	make_uniq<SetVariableStatement>(info.name, std::move(info.parameters[0]), SetScope::AUTOMATIC);
+	return std::move(set_statement);
 }
 
 unique_ptr<SQLStatement> PEGTransformerFactory::TransformPragmaFunction(PEGTransformer &transformer,
@@ -24,7 +29,7 @@ unique_ptr<SQLStatement> PEGTransformerFactory::TransformPragmaFunction(PEGTrans
 	// Rule: PragmaFunction <- PragmaName PragmaParameters?
 	auto result = make_uniq<PragmaStatement>();
 	auto &list_pr = parse_result->Cast<ListParseResult>();
-	result->info->name = transformer.Transform<string>(list_pr.children[0]);
+	result->info->name = list_pr.Child<IdentifierParseResult>(0).identifier;
 	auto &optional_parameters_pr = list_pr.Child<OptionalParseResult>(1);
 	if (optional_parameters_pr.optional_result) {
 		result->info->parameters = transformer.Transform<vector<unique_ptr<ParsedExpression>>>(optional_parameters_pr.optional_result);
