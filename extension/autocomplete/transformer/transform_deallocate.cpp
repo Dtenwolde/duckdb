2fe42af10aa5e84c3f34446f3817b396a5669930
dtenwolde <dljtw98@gmail.com>
2025-08-06 13:31:55 +0200

    Implement Deallocatestatement

---
+++ extension/autocomplete/transformer/transform_deallocate.cpp
@@ -0,0 +1,16 @@
+#include "duckdb/parser/statement/drop_statement.hpp"
+#include "transformer/peg_transformer.hpp"
+
+namespace duckdb {
+
+unique_ptr<SQLStatement> PEGTransformerFactory::TransformDeallocateStatement(PEGTransformer &transformer,
+																		 optional_ptr<ParseResult> parse_result) {
+	auto &list_pr = parse_result->Cast<ListParseResult>();
+
+	auto result = make_uniq<DropStatement>();
+	result->info->type = CatalogType::PREPARED_STATEMENT;
+	auto name = list_pr.Child<IdentifierParseResult>(2).identifier;
+	result->info->name = string(name);
+	return result;}
+
+} // namespace duckdb
