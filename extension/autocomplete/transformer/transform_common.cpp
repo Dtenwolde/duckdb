7392bfe7242b7c796bb0b61b5cf329f88e613fe1
dtenwolde <dljtw98@gmail.com>
2025-08-07 13:53:20 +0200

    Smoll bug

--- extension/autocomplete/transformer/transform_common.cpp
+++ extension/autocomplete/transformer/transform_common.cpp
@@ -11,6 +11,9 @@ string PEGTransformerFactory::TransformColIdOrString(PEGTransformer &transformer
 string PEGTransformerFactory::TransformColId(PEGTransformer &transformer, optional_ptr<ParseResult> parse_result) {
 	auto &list_pr = parse_result->Cast<ListParseResult>();
 	auto &choice_pr = list_pr.Child<ChoiceParseResult>(0);
+	if (choice_pr.result->type == ParseResultType::IDENTIFIER) {
+		return choice_pr.result->Cast<IdentifierParseResult>().identifier;
+	}
 	return transformer.Transform<string>(choice_pr.result);
 }
 
