ca6910f8d727f445db5c36529d67c4f47d18630c
dtenwolde <dljtw98@gmail.com>
2025-08-07 17:37:36 +0200

    Starting on proper handling of operator

---
+++ extension/autocomplete/transformer/transform_operator.cpp
@@ -0,0 +1,15 @@
+#include "duckdb/common/enums/operator_result_type.hpp"
+#include "transformer/peg_transformer.hpp"
+
+namespace duckdb {
+
+ExpressionType PEGTransformerFactory::TransformOperator(PEGTransformer &transformer, optional_ptr<ParseResult> parse_result) {
+	auto &list_pr = parse_result->Cast<ListParseResult>();
+	auto &choice_pr = list_pr.Child<ChoiceParseResult>(0);
+	if (choice_pr.result->type == ParseResultType::OPERATOR) {
+		return OperatorToExpressionType(choice_pr.result->Cast<OperatorParseResult>().operator_token);
+	}
+	return transformer.Transform<ExpressionType>(choice_pr.result);
+}
+
+} // namespace duckdb
